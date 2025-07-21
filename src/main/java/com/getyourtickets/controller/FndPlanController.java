223c87f871f208dbe74f5d41cbc63691e25bc387
Phạm Văn Thành <vanthanh.pham@urekamedia.vn>
2025-07-21 22:56:37 +0700

    #1: update api response

--- src/main/java/com/getyourtickets/controller/FndPlanController.java
+++ src/main/java/com/getyourtickets/controller/FndPlanController.java
@@ -1,10 +1,19 @@
 package com.getyourtickets.controller;
 
+import com.getyourtickets.dto.ApiResponse;
+import com.getyourtickets.dto.FndPlanResponse;
 import com.getyourtickets.mapper.FndPlanMapper;
+import com.getyourtickets.service.CommonService;
 import com.getyourtickets.service.FndPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fnd-plan")
@@ -12,13 +21,23 @@ public class FndPlanController {
     @Autowired
     private FndPlanService fndPlanService;
 
-    @RequestMapping("/getAll")
-    public Object getAllFndPlans() {
+    @GetMapping("/getAll")
+    public ResponseEntity<ApiResponse> getAllFndPlans() {
         try {
-            return fndPlanService.getAllFndPlans();
+            List<FndPlanResponse> responses = fndPlanService.getListFndPlanResponse();
+
+            return new ResponseEntity<>(CommonService.buildResponse(1000, "Success", responses), HttpStatus.OK);
         } catch (Exception e) {
             throw new RuntimeException("Error fetching Fnd Plans: " + e.getMessage());
-//            return "Error fetching Fnd Plans: " + e.getMessage();
         }
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createFndPlan() {
+
+
+        return new ResponseEntity<>("Fnd Plan created successfully", HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/getyourtickets/implement/FndPlanServiceImp.java
+++ src/main/java/com/getyourtickets/implement/FndPlanServiceImp.java
@@ -1,5 +1,6 @@
 package com.getyourtickets.implement;
 
+import com.getyourtickets.dto.FndPlanResponse;
 import com.getyourtickets.mapper.FndPlanMapper;
 import com.getyourtickets.model.FndPlan;
 import com.getyourtickets.service.FndPlanService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FndPlanServiceImp implements FndPlanService {
@@ -17,4 +19,12 @@ public class FndPlanServiceImp implements FndPlanService {
     public List<FndPlan> getAllFndPlans() {
         return fndPlanMapper.getAllFndPlans();
     }
+
+    @Override
+    public List<FndPlanResponse> getListFndPlanResponse() {
+        List<FndPlan> fndPlans = this.getAllFndPlans();
+        return fndPlans.stream().map(p-> FndPlanResponse.from(p)).collect(Collectors.toList());
+    }
+
+
 }
