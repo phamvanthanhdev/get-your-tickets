8510d04209ba232d54b0f479836f746e69ffae9d
Phạm Văn Thành <vanthanh.pham@urekamedia.vn>
2025-07-21 20:12:24 +0700

    init commit
    (cherry picked from commit e7ba099de3d6f1b3ba7ac34d20aa6f05567e3c77)

---
+++ src/main/java/com/getyourtickets/GetyourticketsApplication.java
@@ -0,0 +1,13 @@
+package com.getyourtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetyourticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetyourticketsApplication.class, args);
+	}
+
+}
